a8af8c8d04c6aeb97cd57c9b63c1fb05fffd5d98
Sim Neyzi <simneyzi@gmail.com>
2022-09-12 09:53:01 -0700

    add data validation

--- src/components/Binary.js
+++ src/components/Binary.js
@@ -1,7 +1,25 @@
 import TextField from "@mui/material/TextField";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Binary({ convert }) {
+  const [input, setInput] = useState("");
+
+  const onInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    if (input) {
+      if (input.match(/^[0-1]+$/g) !== null) {
+        console.log("binary");
+        convert(input);
+      } else {
+        alert("Enter either 0 or 1");
+        setInput("");
+      }
+    }
+  }, [input]);
+
   return (
     <form>
       <TextField
@@ -10,7 +28,7 @@ function Binary({ convert }) {
         color="success"
         label="Enter binary number"
         variant="standard"
-        onChange={(e) => convert(e.target.value)}
+        onChange={onInput}
       />
     </form>
   );
